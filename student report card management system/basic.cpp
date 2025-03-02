c40f4e03b21570d8131b65e519592b346d324769
Swap-20 <155949552+Swap-20@users.noreply.github.com>
2025-03-02 17:24:58 +0530

    Add files via upload

---
+++ student report card management system/basic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+using namespace std;
+
+class Student{
+    private:
+        string RollNo, Name, Grade;
+    public:
+        Student(string RollNo, string Name, string Grade){
+        }
+    }
